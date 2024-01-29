f7cdfaef54219fe052549304b56bc3cab8cc3f9a
Daniel Bednarek <Daniel_Bednarek@epam.com>
2024-01-29 16:03:56 +0100

    initial setup cont. typescript, tailwind setup etc.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import AdminPage from './pages/AdminPage/AdminPage'
+
+function App() {
+  return (
+    <main className="bg-main text-text-main">
+      <AdminPage></AdminPage>
+    </main>
+  );
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  theme: {
+    extend: {},
+    colors: {
+      main: '#1b2838',
+      'text-main': '#ffffff'
+    }
+  },
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  plugins: [{
+    tailwindcss: {},
+    autoprefixer: {},
+  }]
 }
 
