fb693c71067a8a8d8460aaf15839c11b1f32d95a
Daniel Bednarek <Daniel_Bednarek@epam.com>
2024-02-01 22:26:47 +0100

    fff

--- src/helpers/mockAPI.ts
+++
@@ -1,9 +0,0 @@
-export default async function mockAPI(jsonData: any) {
-	return new Promise(function (resolve, reject) {
-		try {
-			resolve(jsonData);
-		} catch (e: any) {
-			reject(new Error(e.message));
-		}
-	});
-}
